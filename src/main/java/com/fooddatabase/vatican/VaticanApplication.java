8690874cc0806163a4e5c7d556fda45891906e3c
ian <adriansalut@ymail.com>
2022-06-10 18:12:36 +1200

    le initial commit

---
+++ src/main/java/com/fooddatabase/vatican/VaticanApplication.java
@@ -0,0 +1,13 @@
+package com.fooddatabase.vatican;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaticanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaticanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fooddatabase/vatican/VaticanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddatabase.vatican;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaticanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
